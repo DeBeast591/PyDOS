d592712b213202520564cbec978ea37f5611a170
DeBeast591 <52985378+DeBeast591@users.noreply.github.com>
2020-02-18 20:40:27 -0500

    Add files via upload

--- PyDOS.py
+++ PyDOS.py
@@ -47,11 +47,11 @@ def config():
     primary_drive = CONFIG[2]
 
     # Config Variables (Bool)
-    if CONFIG[3] == "true" or "true\n": show_stats = True
+    if CONFIG[3] in ["true", "true\n"]: show_stats = True
     else: show_stats = False
-    if CONFIG[4] == "true" or "true\n": ask_for_drives = True
+    if CONFIG[4] in ["true", "true\n"]: ask_for_drives = True
     else: ask_for_drives = False
-    if CONFIG[4] == "true" or "true\n": allow_config_edits = True
+    if CONFIG[4] in ["true", "true\n"]: allow_config_edits = True
     else: allow_config_edits = False
 
 # Start Code
@@ -93,6 +93,9 @@ if ask_for_drives:
         else: print("E: Drive does not exist.")
 else:
     ACTIVE_DRIVE = primary_drive
+    if "\n" in ACTIVE_DRIVE:
+        ACTIVE_DRIVE = ACTIVE_DRIVE.strip("\n")
+        primary_drive = primary_drive.strip("\n")
     MAIN_FILES = PATH + "/DRIVES/" + ACTIVE_DRIVE
 
 # Drive Config
@@ -153,12 +156,14 @@ while True:
 
     # help
     if action[0] == "help":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             print("PyDOS Help:")
-            print("NOTE: Printed in order from where they are in the source code\n")
+            print("NOTE: Printed in order from where they are in the source code")
+            print("NOTE: Some commands are blocked by read/only drives\n")
             print("help - prints help")
             print("lf - prints all folders and files in the active folder")
             print("ld - prints all drives")
+            print("ls - lists files, folders, and drives (only lists files and folders in the active drive)")
             print("end {OPTIONAL_FLAG} - terminates PyDOS")
             print("\t-y - auto-ends the session without asking")
             print("open {FLAG} {FILE} - opens a file and runs it or prints it's contents")
@@ -173,7 +178,8 @@ while True:
             print("\t-dir - makes a dir")
             print("\t-folder - makes a dir")
             print("\t-file - makes a file and edits using Nano")
-            print("del {FILE} - deletes a file (moves it to DRIVES/.trash)")
+            print("\t-drive - makes a drive")
+            print("del {FILE} - deletes a file (moves it to DRIVES/.trash/)")
             print("config {FLAG} - edits config")
             print("\t-main - main config")
             print("\t-drive - drive config")
@@ -181,34 +187,53 @@ while True:
             print("reload - reloads the config and drive config")
             print("echo {ANY_STRING} - prints the '{ANY_STRING}'")
             print("cls - clears the screen")
+            print("contition {TYPE} {SAY} {WANTED_INPUT} {TRUE_OUTPUT} {FALSE_OUTPUT} - true / false")
+            print("\tTYPES:")
+            print("\t\tinput - gets user input")
+            print("\t\toutput - get the last output (TRUE/FALSE OUTPUT)")
+            print("\tNOTES:")
+            print("\t\t{WANTED_INPUT} - not required for the output type")
+            print("etrash - emptys the trash (DRIVES/.trash/)")
 
     # lists all files/folders
     elif action[0] == "lf":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             files = os.listdir(PATH + "/DRIVES/" + ACTIVE_DRIVE)
             for x in files:
                 print(x)
 
     # lists all drives
     elif action[0] == "ld":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             drives = os.listdir(PATH + "/DRIVES/")
             for x in drives:
+                print(x)
+    
+    # lists all folders, files, and drives
+    elif action[0] == "ls":
+        if not command_check(action, 1):
+            drives = os.listdir(PATH + "/DRIVES/")
+            print("Drives: ")
+            for x in drives:
+                print(x)
+            print("\nFolders and Files:")
+            files = os.listdir(PATH + "/DRIVES/" + ACTIVE_DRIVE)
+            for x in files:
                 print(x)
 
     # terminates PY-DOS
     elif action[0] == "end":
-        if command_check(action, len(action)) == False:
+        if not command_check(action, len(action)):
             if len(action) == 2:
                 if action[1] == "-y":
                     quit()
             else:
                 if input("Are you sure you want to end this session? (y/n)\n\t") == "y": quit()
-                else: print("Okay")
+                else: print("Abort")
 
     # opens files
     elif action[0] == "open":
-        if command_check(action, 3) == False:
+        if not command_check(action, 3):
             # prints files
             if os.path.exists(PATH + "/DRIVES/" + ACTIVE_DRIVE + "/" + action[2]):
                 if action[1] == "-print":
@@ -234,7 +259,7 @@ while True:
     elif action[0] == "cd":
         # cd to drives
         if action[1] == "-d":
-            if command_check(action, 3) == False:
+            if not command_check(action, 3):
                 if os.path.exists(PATH + "/DRIVES/" + action[2]):
                     if "/" in ACTIVE_DRIVE:
                         print("E: You must be in the root of the drive to use this command.")
@@ -272,7 +297,7 @@ while True:
             else: print("Drive does not exist.")
         # go up in folder tree
         elif action[1] == "..":
-            if command_check(action, 2) == False:
+            if not command_check(action, 2):
                 LOCATION = str(os.getcwd())
                 OUTSIDE_DRIVE = False
                 if ACTIVE_DRIVE == "" or " " or "/":
@@ -290,7 +315,7 @@ while True:
 
     # makes a dir or file (can also edit files)
     elif action[0] == "mk":
-        if command_check(action, 3) == False:
+        if not command_check(action, 3):
             if MODE == "READ_ONLY": print("E: This is a read only drive.")
             else:
                 if action[1] in ["-dir", "-folder"]:
@@ -301,13 +326,13 @@ while True:
 
     # deletes a dir and puts it in the trash
     elif action[0] == "del":
-        if command_check(action, 2) == False:
+        if not command_check(action, 2):
             if MODE == "READ_ONLY": print("E: This is a read only drive.")
             else: os.system("mv " + PATH + "/DRIVES/" + ACTIVE_DRIVE + "/" + action[1] + " " + PATH + "/DRIVES/.trash/")
 
     # config
     elif action[0] == "config":
-        if command_check(action, 2) == False:
+        if not command_check(action, 2):
             if action[1] == "-main":
                 if allow_config_edits: os.system("nano " + PATH + "/data/config.txt")
                 else: print("E: Config edits are blocked.")
@@ -317,34 +342,60 @@ while True:
 
     # config and drive config reload
     elif action[0] == "reload":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             config()
             drive_config()
 
     # sleep
     elif action[0] == "sleep":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             action = input(input_marker)
             hide_stat = True
-            pass
 
     # echo
     elif action[0] == "echo":
-        if command_check(action, len(action)) == False:
+        if not command_check(action, len(action)):
             action.remove("echo")
             action = " ".join(action)
             print(action)
 
     # cls
     elif action[0] == "cls":
-        if command_check(action, 1) == False:
+        if not command_check(action, 1):
             os.system("clear")
-
+    
+    # conditions
+    elif action[0] == "condition":
+        if not command_check(action, 1):
+            # gets user input and tests
+            if action[1] == "input":
+                if input(action[2] + input_marker) == action[3]:
+                    CONDITION_OUTPUT = action[4]
+                else:
+                    CONDITION_OUTPUT = action[5]
+            # gets the previous condition's output and tests
+            elif action[1] == "output":
+                if CONDITION_OUTPUT == action[2]:
+                    CONDITION_OUTPUT = action[3]
+                    print(action[3])
+                else:
+                    CONDITION_OUTPUT = action[4]
+                    print(action[4])
+    
+    # empty trash
+    elif action[0] == "etrash":
+        if not command_check(action, 1):
+            if MODE == "READ_WRITE":
+                if input("Are you sure, this CAN NOT be undone? (y/n) ") == "y":
+                    os.system("rm -rf " + PATH + "/DRIVES/.trash/*")
+                else:
+                    print("Abort")
+            else: print("E: This is a read only drive.")
+    
     # error
     else:
         print("E: Command does not exist.")
 
     # show stats
     if show_stats:
-        if RunningPyDOSFile: pass
-        else: print("\n[STATS] Executed")
+        if not RunningPyDOSFile: print("\n[STATS] Executed")
